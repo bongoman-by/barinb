49fb039544972df8712242f0cb0e4217bc106730
bongoman-by <bongoman@tut.by>
2021-11-09 17:15:22 +0300

    feat(app): test services
    *	modified:   jest.config.js
    *	modified:   package-lock.json
    *	modified:   package.json
    *	new file:   src/app/booking/service/booking.service.spec.ts
    *	new file:   src/app/booking/state/booking.stub.ts
    *	modified:   src/app/booking/state/index.ts
    *	new file:   src/app/buildings/state/building.stub.ts
    *	modified:   src/app/buildings/state/index.ts
    *	modified:   src/app/core/navigation/service/navigation.service.ts
    *	new file:   src/app/core/storage/storages/local.storage.spec.ts
    *	new file:   src/app/core/store/utils/creator.util.ts
    *	modified:   src/app/core/store/utils/index.ts
    *	new file:   src/app/core/testing/index.ts
    *	new file:   src/app/core/testing/page.object.ts
    *	new file:   src/app/core/testing/provider-of.util.ts
    *	modified:   src/app/persons/state/index.ts
    *	new file:   src/app/persons/state/person.stub.ts
    *	modified:   src/app/rooms/manager/index.ts
    *	new file:   src/app/rooms/manager/room-manager.stub.ts
    *	new file:   src/app/rooms/manager/room.manager.spec.ts
    *	new file:   src/app/rooms/page/components/room-booking-card/components/room-booking-price/room-booking-price.service.spec.ts
    *	new file:   src/app/rooms/service/room.service.spec.ts
    *	modified:   src/app/rooms/state/index.ts
    *	new file:   src/app/rooms/state/room.stub.ts
    *	modified:   tsconfig.json
    *	modified:   yarn.lock
    
    Closes #52

--- src/app/core/store/utils/index.ts
+++ src/app/core/store/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './data-persistence.util';
 export * from './payload.util';
 export * from './testing.util';
+export * from './creator.util';
--- src/app/rooms/state/index.ts
+++ src/app/rooms/state/index.ts
@@ -1,2 +1,3 @@
 export * from './room-state.module';
+export * from './room.stub';
 export * from './room.facade';
