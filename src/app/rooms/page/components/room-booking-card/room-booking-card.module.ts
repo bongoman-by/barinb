3b57e1406b07dc4ecde52792f6bdf70b44c55869
bongoman-by <bongoman@tut.by>
2021-11-11 10:43:04 +0300

    feat(room): add tests of components
    * modified:   package.json
    * modified:   src/app/rooms/page/components/room-booking-card/components/room-booking-form/room-booking-form.component.spec.ts
    * modified:   src/app/rooms/page/components/room-booking-card/components/room-booking-form/room-booking-form.module.ts
    * new file:   src/app/rooms/page/components/room-booking-card/components/room-booking-form/room-booking-form.po.ts
    * modified:   src/app/rooms/page/components/room-booking-card/room-booking-card.component.spec.ts
    * modified:   src/app/rooms/page/components/room-booking-card/room-booking-card.module.ts
    * new file:   src/app/rooms/page/components/room-booking-card/room-booking-card.po.ts
    * modified:   src/app/rooms/page/components/room-details/room-details.component.html
    * modified:   src/app/rooms/page/components/room-details/room-details.component.spec.ts
    * modified:   src/app/rooms/page/components/room-details/room-details.module.ts
    * new file:   src/app/rooms/page/components/room-details/room-details.po.ts
    
    Closes #54

--- src/app/rooms/page/components/room-booking-card/room-booking-card.module.ts
+++ src/app/rooms/page/components/room-booking-card/room-booking-card.module.ts
@@ -7,10 +7,11 @@ import { NumeralSharedModule } from '@app/core/numerals/shared';
 import { RoomBookingFormModule } from './components/room-booking-form/room-booking-form.module';
 import { RoomBookingPriceModule } from './components/room-booking-price/room-booking-price.module';
 import { RoomBookingCardComponent } from './room-booking-card.component';
+import { WrapperComponent } from './room-booking-card.component.spec';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, NumeralSharedModule, RoomBookingFormModule, RoomBookingPriceModule],
-  declarations: [RoomBookingCardComponent],
-  exports: [RoomBookingCardComponent],
+  declarations: [RoomBookingCardComponent, WrapperComponent],
+  exports: [RoomBookingCardComponent, WrapperComponent],
 })
 export class RoomBookingCardModule {}
